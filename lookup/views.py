c5abb10062d64a0a9ef0710b404ec521d7118862
Marmik Patel <marmikpatelme13398@gmail.com>
2020-05-23 09:06:14 +0530

    setup api in views.py and display weather data on home page.

--- lookup/views.py
+++ lookup/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+import json
+import requests
 
 # Create your views here.
 
 
 def home(request):
-	return render(request, 'home.html')
+	params = {
+	'access_key': '31efc493add11887495530e3985d51b5',
+	'type': 'Zipcode',
+	'query': 'Mehsana'
+}
+	api_request = requests.get('http://api.weatherstack.com/current', params)
+
+
+	try:
+		api = api_request.json()
+	except Exception as e:	
+		api = "Error..."
+	return render(request, 'home.html', {'api': api})
 
 def about(request):
 	return render(request, 'about.html')
